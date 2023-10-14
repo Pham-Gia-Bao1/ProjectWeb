b33b59a5ee976c0631f13fdcfd68224a6b0d84ec
hue <hue.ho25@student.passerellesnumeriques.org>
2023-10-14 10:17:42 +0700

    edit update customer

--- services/admin_customer.js
+++ services/admin_customer.js
@@ -3,7 +3,9 @@ function fetchCustomers() {
     .then(response => response.json())
     .then(data => {
       var listCustomerTable = document.getElementById('list_customer');
-      var tableBody = listCustomerTable.createTBody();
+      var tableBody = listCustomerTable.tBodies[0];
+      tableBody.innerHTML = ""; // Clear the table body
+
       data.forEach(customer => {
         var row = tableBody.insertRow();
         row.innerHTML = `
@@ -13,26 +15,28 @@ function fetchCustomers() {
           <td>${customer.phoneNumber}</td>
           <td>${customer.address}</td>
           <td>
-          <button id="update_btn_product" onclick="update_customer(${customer.id})">Update</button>
-            <button id="delete_btn_product" onclick="delete_customer(${customer.id})">Delete</button>
-         
+            <button onclick="update_customer(${customer.id})">Update</button>
+            <button onclick="delete_customer(${customer.id})">Delete</button>
           </td>
         `;
       });
     });
 }
+
 fetchCustomers();
 
 const customerForm = document.getElementById("cusForm");
-customerForm.addEventListener("submit", createCustomer);
-function createCustomer (){
-  console.log("Quy cute fo mai que");
+customerForm.addEventListener("submit", createOrUpdateCustomer);
+
+function createOrUpdateCustomer(event) {
+  event.preventDefault();
   const name = document.getElementById("name").value;
   const password = document.getElementById("password").value;
   const phone = document.getElementById("phone").value;
   const email = document.getElementById("email").value;
   const roleId = document.getElementById("role_id").value;
   const address = document.getElementById("address").value;
+
   const customer = {
     name: name,
     password: password,
@@ -41,84 +45,84 @@ function createCustomer (){
     roleId: roleId,
     address: address
   };
-  localStorage.setItem("customer", JSON.stringify(customer));
 
-  fetch("http://localhost:3000/customer", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(customer)
-  })
-  .then(response => {
-    if (response.ok) {
-      console.log("Customer added successfully!");
-    } else {
-      console.log("Failed to add customer.");
-    }
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
-};
-delete customer
+  const customerId = localStorage.getItem("customer_id");
+
+  if (customerId) {
+    // Update existing customer
+    fetch(`http://localhost:3000/customer/${customerId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(customer)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Customer updated successfully!");
+          localStorage.removeItem("customer_id"); // Remove the stored customer ID
+        } else {
+          console.log("Failed to update customer.");
+        }
+        fetchCustomers(); // Refresh the customer table
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  } else {
+    // Create new customer
+    fetch("http://localhost:3000/customer", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(customer)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Customer added successfully!");
+          fetchCustomers(); // Refresh the customer table
+        } else {
+          console.log("Failed to add customer.");
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }
+
+  // Reset the form
+  customerForm.reset();
+}
+
 function delete_customer(id) {
   fetch(`http://localhost:3000/customer/${id}`, {
     method: "DELETE",
   })
     .then(() => {
-      fetchCustomers(); // Gọi lại hàm fetchCustomers() để cập nhật danh sách khách hàng sau khi xóa
+      fetchCustomers(); // Refresh the customer table
       alert("Delete success");
     })
     .catch(() => {
       alert("Delete fail");
     });
 }
+
 function update_customer(id) {
-  
   fetch(`http://localhost:3000/customer/${id}`)
     .then(response => response.json())
     .then(customer => {
-      
-      document.getElementById('name').value = customer.name;
-      document.getElementById('password').value = customer.password;
-      document.getElementById('phone').value = customer.phoneNumber;
-      document.getElementById('email').value = customer.email;
-      document.getElementById('role_id').value = customer.role_id;
-      document.getElementById('address').value = customer.address;
+      document.getElementById("name").value = customer.name;
+      document.getElementById("password").value = customer.password;
+      document.getElementById("phone").value = customer.phoneNumber;
+      document.getElementById("email").value = customer.email;
+      document.getElementById("role_id").value = customer.role_id;
+      document.getElementById("address").value = customer.address;
 
-      
-      var modal = new bootstrap.Modal(document.getElementById('myModal'));
+      localStorage.setItem("customer_id", id); // Store the customer ID
+
+      var modal = new bootstrap.Modal(document.getElementById("myModal"));
       modal.show();
-
-      
-      document.getElementById('cusForm').onsubmit = function(event) {
-        event.preventDefault();
-        var updatedCustomer = {
-          name: document.getElementById('name').value,
-          password: document.getElementById('password').value,
-          phoneNumber: document.getElementById('phone').value,
-          email: document.getElementById('email').value,
-          role_id: document.getElementById('role_id').value,
-          address: document.getElementById('address').value
-        };
-
-        
-        fetch(`http://localhost:3000/customer/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(updatedCustomer)
-        })
-          .then(() => {
-            fetchCustomers(); // Refresh the customer table
-            modal.hide(); // Hide the modal
-          })
-          .catch(() => {
-            alert("Update fail");
-          });
-      };
     })
     .catch(() => {
       alert("Error retrieving customer data");
